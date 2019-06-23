9f6df83ed9d6c689a470ccb868f00e3b6ca5a184
Denis Toledo <contato@denistoledo.com.br>
2019-06-22 23:45:55 -0300

    add gulp + sass compiler + browsersync

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const gulp = require('gulp')
+const { watch, parallel } = require('gulp')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')
+
+sass.compiler = require('node-sass')
+
+const watcher = watch(['index.html', 'scss/app.scss'])
+
+watcher.on('change', function(path, stats) {
+  console.log(`File ${path} was changed`);
+  return (path === 'index.html') ? browserSync.reload() : sassCompiler()
+})
+
+function sassCompiler() {
+  return gulp
+    .src('./scss/app.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(gulp.dest('css/app.min.css'))
+    .pipe(browserSync.reload({
+			stream: true
+		}))
+}
+
+function server() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  })
+}
+
+exports.default = parallel(sassCompiler, server)
